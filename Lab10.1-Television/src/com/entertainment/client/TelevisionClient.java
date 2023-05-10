41ac77f7d102936135517ea2d89c45d2b10719a8
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-05-10 13:26:34 -0600

    Implemented throws IllegalArgumentException

---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClient.java
@@ -0,0 +1,10 @@
+package com.entertainment.client;
+
+import com.entertainment.Television;
+
+class TelevisionClient {
+    public static void main(String[] args) {
+        Television tv1 = new Television("Samsung", 32);
+        System.out.println(tv1);
+    }
+}
